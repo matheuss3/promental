1c17d69f2aa19d84491b1d96d3d9e7ba53403263
Matheus Oliveira <56975589+matheuss3@users.noreply.github.com>
2022-08-05 01:03:20 -0300

    update repo

--- web/app.py
+++ web/app.py
@@ -1,30 +1,57 @@
-from ast import Add
-from pycaret.classification import load_model, predict_model
+import pandas as pd
+from joblib import load
 import streamlit as st
 
-model = load_model('deployment_26072022')
+import json
+
+model = load("random_forest_sklearn.joblib")
 
 def predict(model, input_df):
-    predictions_df = predict_model(extimator=model, data=input_df)
-    predictions = predictions_df['Label'][0]
+    # predictions_df = predict_model(estimator=model, data=input_df)
+    predictions = model.predict(input_df)
     return predictions
 
+def create_question(description, options):
+    options_fmt = []
+    for key, value in options.items():
+        # if value == "":
+        #     options_fmt.append(f"{key}")
+        # else:
+        options_fmt.append(f"{value} - {key}")
+
+
+    question = st.selectbox(description, options_fmt)
+    return question
 
 def run():
+    questions = json.load(open("questions.json", encoding="utf8"))
     st.write("""
-    # Promental""")
+    # Questionário""")
 
 
+    st.sidebar.write("""
+    # 🧠 Promental""")
+
+    
     add_selectbox = st.sidebar.selectbox(
-        "Como gostaria de prever?", 
+        "Como gostaria realizar o teste?", 
         ("Online", "Bacht"))
     
-    if add_selectbox == "Online":
-        age = st.number_input("Qual sua idade?", min_value=0, max_value=100, value=18)
-        if st.checkbox("Fumante"):
-            fumante = 1
-        else:
-            fumante = 5
     
+    if add_selectbox == "Online":
+        input_dict = {}
+        for question in questions:
+            input_dict[question["column"]] = int(create_question(question["question"], question["options"]).split(" - ")[0])
+        
+
+        input_df = pd.DataFrame([input_dict])
+        print(input_df)
+        output = ""
+
+        if st.button("Finalizar"):
+            output = predict(model=model, input_df=input_df)
+            output = '$' + str(output)
+        
+        st.success('O resultado é {}'.format(output))
 
 run()
